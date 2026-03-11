86164ce506c29fc86f17ebe02f2d627fc8f80dd2
dooumhoo <kimdong1776@gmail.com>
2026-03-11 11:23:26 +0900

    2차시

--- 실습2/src/실습2/homework.java
+++ 실습2/src/실습2/homework.java
@@ -1,3 +1,25 @@
+
+/* scnaf 쓰는 법. ▽
+		* Scanner input;
+		* input = new Scanner (System.in);
+		* 
+		* Scanner input = new Scanner (System.in);sc
+		*/
+		/*
+ * 변수 (variable)
+		 * =>변핤 있는 수? (x) from 수학
+		 * =>데이터를 저장하는 공간(on Memory)!
+		 *  built in variable and Reference variable(빌트인이 아닌 거 모두)
+		 *  빌트인은 byte short int long float double char boolean
+		 *  레퍼런스는 Scanner(4 or 8 Bytes) String  Array Cloass Object 
+		 *  	레퍼런스는 모두다 포인터 타입으로 생각해라 근데 자바는 포인터라는 개념이 없어서 포인터 연산이 안됨
+		 *  	Java 는 reference만 함 따라서 C의 포인터 보다 제한 됨.
+		 *  Scanner input = new Scanner (System.in);
+		 *  input은 포인터형 new는 malloc과 거의 유사함.
+		 */
+// 조건식에 boolean 데이터 타입만 들어갈 수 있음. 
+
+
 package 실습2;
 
 import java.util.Scanner;
@@ -12,12 +34,7 @@ public class homework {
 			i++;
 			System.out.printf("%d\n", i);
 		}
-		/* scnaf 쓰는 법. ▽
-		* Scanner input;
-		* input = new Scanner (System.in);
-		* 
-		* Scanner input = new Scanner (System.in);
-		*/
+		
 		int a;
 		int b = 10;
 		int c;
