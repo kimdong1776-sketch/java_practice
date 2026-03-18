0ef5922b8fd4d8a33e639bdcbe58bc06dc6f6300
dooumhoo <kimdong1776@gmail.com>
2026-03-18 11:37:15 +0900

    4차신가? 3-18일

--- 실습3/src/실습3/Student.java
+++ 실습3/src/실습3/Student.java
@@ -2,12 +2,13 @@ package 실습3;
 
 public class Student {
 	private String name;
+	private double average;
+	private float grade;
 	/*
 	 * 그냥 직접 접근하게 두면 편할 텐데, 왜 굳이 private으로 막고 메서드를 통하게 할까요? 바로 '데이터 보호' 때문입니다.
 	 * 예를 들어, average 점수는 0점보다 낮거나 100점보다 높을 수 없죠? 만약 private이 아니라면
 	 * 외부에서 실수로 -500점을 넣어도 막을 방법이 없습니다. 하지만 메서드를 통하면 필터링을 할 수 있어요.
 	 */
-	private double average;
 	
 	void setName(String _name) {
 		this.name = _name;
@@ -23,5 +24,19 @@ public class Student {
 	
 	public double getAverage() {
 		return this.average;
+	}
+	
+	public double getScore(double ...score) { 	
+		double total = 0.0;
+		for(double i : score) {
+			total += i;
+		}
+		return total;
+	}
+	public void setGrade(float _grade) {
+		this.grade = _grade;
+	}
+	public float getGrade() {
+		return this.grade;
 	}
 }
--- 실습3/src/실습3/StudentTest.java
+++ 실습3/src/실습3/StudentTest.java
@@ -4,21 +4,31 @@ public class StudentTest {//클래스의 필요성은 구조체의 필요성과 
 							//사용자 정의 자료형으로, 데이터의 체계적인 그룹화, 가독성 향상, 
 							//코드의 재사용성 및 유지보수 편의성을 위해 필수적
 	public static void main(String[] args) {
-		double a;
-		String str;
 		Student s; //stack
+		Student s1;
 		s = new Student(); //heap
+		s1 = new Student();
+		
+		
 		s.setAverage(3.4);
-		s.setName("김동후");//C에선 요정도로 잘 돌아간다(name, strcpy쓰기). 하지만 java는 레퍼런스 변수기 떄문에 안 됨
+		s.setName("김동후");
+		s.setGrade(3.9f);
+		
+
+		s1.setAverage(3.51);
+		s1.setName("홍길동");
+		s1.setGrade(4.1f);//C에선 요정도로 잘 돌아간다(name, strcpy쓰기). 하지만 java는 레퍼런스 변수기 떄문에 안 됨
 		//따라서 s = new Student();를 추가해준다. new는 메모리 할당 student 클래스를 할당 후 그 주소값을 s에 저장
 		// Student 가 클래스 s가 가르키는 것이 객체(메모리).
 		
-		a= s.getAverage();
-		str = s.getName();
 		
-		System.out.println("a = " +a);
-		System.out.println("name = " +str);
+		System.out.println("==================정보====================");
+		System.out.printf("%7s %7s %13s %9s\n", "name", "grade", "total Score", "average");
+		System.out.printf("%6s %5.2f/4.3 %8.2f %10.2f\n", s.getName(), s.getGrade(), s.getScore(5,6,8,7,9,8,10,20,55.56), s.getAverage());
+		System.out.printf("%6s %5.2f/4.3 %8.2f %10.2f\n", s1.getName(), s1.getGrade(), s1.getScore(5,6,8,7,9,8,10,20,55.56), s1.getAverage());
+		System.out.println("=========================================\n");
 		
+	
 		System.out.println("Bye...");
 		
 	}
